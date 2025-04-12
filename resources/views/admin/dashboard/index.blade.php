afba86b7d48de3415f9c342f75555b31d1a592ec
erik-cahya <erik.cahya841@gmail.com>
2025-04-12 18:35:35 +0800

    update (dashboard) : delete comment and mark code

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,23 +1,16 @@
 @extends('admin.layouts.master')
 @section('css_page')
-    <!-- Daterangepicker css -->
     <link rel="stylesheet" href="{{ asset('velonic_admin') }}/assets/vendor/daterangepicker/daterangepicker.css">
-    <!-- Vector Map css -->
     <link rel="stylesheet" href="{{ asset('velonic_admin') }}/assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css">
-    <!-- Theme Config Js -->
-    <script src="{{ asset('velonic_admin') }}/assets/js/config.js"></script>
-    <!-- App css -->
     <link href="{{ asset('velonic_admin') }}/assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
-    <!-- Icons css -->
     <link href="{{ asset('velonic_admin') }}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <script src="{{ asset('velonic_admin') }}/assets/js/config.js"></script>
 @endsection
 
 @section('content')
 <div class="content">
-    <!-- Start Content-->
     <div class="container-fluid">
 
-        <!-- start page title -->
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box">
@@ -31,7 +24,6 @@
                 </div>
             </div>
         </div>
-        <!-- end page title -->
 
         <div class="row">
             <div class="col-xxl-3 col-sm-6">
@@ -45,7 +37,7 @@
                         <h3 class="my-2">{{ $countAsesor }} <span class="text-uppercase fs-14 ">/ Orang</span></h3>
                     </div>
                 </div>
-            </div> <!-- end col-->
+            </div>
 
             <div class="col-xxl-3 col-sm-6">
                 <div class="card widget-flat text-bg-purple">
@@ -58,7 +50,7 @@
                         <h3 class="my-2">{{ $countTUK }} <span class="text-uppercase fs-14 ">/ TUK</span></h3>
                     </div>
                 </div>
-            </div> <!-- end col-->
+            </div> 
 
             <div class="col-xxl-3 col-sm-6">
                 <div class="card widget-flat text-bg-info">
@@ -71,7 +63,7 @@
                         <h3 class="my-2">{{ $countSkema }} <span class="text-uppercase fs-14 ">/ Skema</span></h3>
                     </div>
                 </div>
-            </div> <!-- end col-->
+            </div> 
 
             <div class="col-xxl-3 col-sm-6">
                 <div class="card widget-flat text-bg-primary">
@@ -84,15 +76,9 @@
                         <h3 class="my-2">{{ $countAsesi }} <span class="text-uppercase fs-14 ">/ Orang</span></h3>
                     </div>
                 </div>
-            </div> <!-- end col-->
+            </div> 
         </div>
-
-
-        
-
-
     </div>
-    <!-- container -->
 
 </div>
 @endsection
